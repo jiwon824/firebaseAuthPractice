fac5659fad7d802b2693370d02543a95803203af
jiwon <love990890@naver.com>
2025-12-27 17:30:21 +0900

    스프링 부트 starter 업로드

---
+++ firebasePracticeServer/src/main/java/com/jiwon/firebasePracticeServer/FirebasePracticeServerApplication.java
@@ -0,0 +1,13 @@
+package com.jiwon.firebasePracticeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebasePracticeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebasePracticeServerApplication.class, args);
+	}
+
+}
